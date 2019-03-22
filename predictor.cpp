2c9b6149a759326e568c3044106e007749c5bb45
abhiutd <abhibits2012@gmail.com>
2019-03-22 15:49:03 -0500

    update

--- predictor.cpp
+++ predictor.cpp
@@ -48,21 +48,14 @@ class Predictor {
 };
 
 Predictor::Predictor(const string &model_file, int batch, torch::DeviceType mode) {
-  /* Load the network. */
-  // In pytorch, a loaded module in c++ is given 
-  // type torch::jit::script::Module as it has been
-  // ported from python/c++ via pytorch's JIT compiler
+  
+  // Load the network
   net_ = torch::jit::load(model_file);
   assert(net_ != nullptr);
   mode_ = mode;
 
-  // TODO should fetch width and height from model
-  //const torch::detail::OrderedDict<std::string, torch::jit::script::NamedModule>& net_module_dict = net_->get_modules();
-  //size_t net_module_dict_size = net_module_dict.size();
-  //CHECK((int)net_module_dict_size == 1) << "Number of modules - " << (int)net_module_dict_size;  
-  //const torch::jit::script::NamedModule& temp = net_module_dict.get("fc1"); 
-  //temp.module->get_method("fc1_script");  
-
+  // Input shape hard coded for now due to lack of layer shape accessor API
+  // Preferred alternate: pass input layer shape as an input 
   width_ = 224;
   height_ = 224;
   channels_ = 3;
@@ -79,38 +72,25 @@ void Predictor::Predict(float* inputData) {
   at::Tensor tensor_image = torch::from_blob(inputData, at::IntList(sizes), options);
 
   std::vector<torch::jit::IValue> inputs;
-
-  // check if mode is set to GPU
   if(mode_ == torch::kCUDA) {
-    // port model to GPU
     net_->to(at::kCUDA);
-    // port input to GPU
     at::Tensor tensor_image_cuda = tensor_image.to(at::kCUDA);
-    // emplace IValue input
     inputs.emplace_back(tensor_image_cuda);
-    // execute model
     result_ = net_->forward(inputs).toTensor();
   }else {
-    // emplace IValue input
     inputs.emplace_back(tensor_image);
-    // execute model
     result_ = net_->forward(inputs).toTensor();
   }
-  
-  // port output back to CPU
   result_ = result_.to(at::kCPU);
 
 }
 
-PredictorContext NewPytorch(char *model_file, int batch,
-                          int mode) {
+PredictorContext NewPytorch(char *model_file, int batch, int mode) {
+  
   try {
     torch::DeviceType mode_temp{at::kCPU};
-    if (mode == 1) {
-      mode_temp = at::kCUDA;
-    }
-    const auto ctx = new Predictor(model_file, batch,
-                                   (torch::DeviceType)mode_temp);
+    if (mode == 1) mode_temp = at::kCUDA;
+    const auto ctx = new Predictor(model_file, batch, (torch::DeviceType)mode_temp);
     return (void *)ctx;
   } catch (const std::invalid_argument &ex) {
     LOG(ERROR) << "exception: " << ex.what();
@@ -121,33 +101,35 @@ PredictorContext NewPytorch(char *model_file, int batch,
 }
 
 void SetModePytorch(int mode) {
-  if(mode == 1) {
-    // TODO set device here ?
-    torch::Device device(torch::kCUDA);
-  }
+  if(mode == 1) torch::Device device(torch::kCUDA);
 }
 
 void InitPytorch() {}
 
 void PredictPytorch(PredictorContext pred, float* inputData) {
+  
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return;
   }
   predictor->Predict(inputData);
   return;
+
 }
 
 const float*GetPredictionsPytorch(PredictorContext pred) {
+
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return nullptr;
   }
 
   return predictor->result_.data<float>();
+
 }
 
 void DeletePytorch(PredictorContext pred) {
+
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return;
@@ -158,38 +140,47 @@ void DeletePytorch(PredictorContext pred) {
     predictor->prof_ = nullptr;
   }
   delete predictor;
+
 }
 
 int GetWidthPytorch(PredictorContext pred) {
+
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return 0;
   }
   return predictor->width_;
+
 }
 
 int GetHeightPytorch(PredictorContext pred) {
+
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return 0;
   }
   return predictor->height_;
+
 }
 
 int GetChannelsPytorch(PredictorContext pred) {
+
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return 0;
   }
   return predictor->channels_;
+
 }
 
 int GetPredLenPytorch(PredictorContext pred) {
+
   auto predictor = (Predictor *)pred;
   if (predictor == nullptr) {
     return 0;
   }
   predictor->pred_len_ = predictor->result_.size(1);
   return predictor->pred_len_;
+
 }
 
