166c3eed37498e82e9ee7f1d7681d372664a04e5
abhiutd <abhibits2012@gmail.com>
2019-03-22 17:07:11 -0500

    update

--- lib.go
+++ lib.go
@@ -1,7 +1,7 @@
 package pytorch
 
 // #cgo CXXFLAGS: -std=c++11 -I${SRCDIR}/cbits -O3 -Wall -g -Wno-sign-compare -Wno-unused-function  -I/opt/libtorch/include -I/opt/libtorch/include/torch/csrc/api/include
-// #cgo LDFLAGS: -lstdc++ -L/opt/libtorch/lib -ltorch -lcaffe2 -lc10 -lgomp
+// #cgo LDFLAGS: -lstdc++ -L/opt/libtorch/lib -ltorch -lcaffe2 -lc10 -lgomp -lglog
 // #cgo linux,amd64,!nogpu CXXFLAGS: -I/usr/local/cuda/include
 // #cgo linux,amd64,!nogpu LDFLAGS: -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudnn -L/opt/libtorch/lib -lcaffe2_gpu -lcudart -lnvrtc-builtins -lnvrtc -lnvToolsExt -lcuda
 import "C"
