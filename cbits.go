d9ecd15f12d4570448f4d3455b2e6a99d62dd1f2
abhiutd <abhibits2012@gmail.com>
2019-03-25 13:07:06 -0500

    update

--- cbits.go
+++ cbits.go
@@ -124,31 +124,31 @@ func (p *Predictor) Close() {
 }
 
 func (p *Predictor) StartProfiling(name, metadata string) error {
-  cname := C.CString(name)
-  cmetadata := C.CString(metadata)
-  defer C.free(unsafe.Pointer(cname))
-  defer C.free(unsafe.Pointer(cmetadata))
-  C.StartProfilingPytorch(p.ctx, cname, cmetadata)
-  return nil
+	cname := C.CString(name)
+	cmetadata := C.CString(metadata)
+	defer C.free(unsafe.Pointer(cname))
+	defer C.free(unsafe.Pointer(cmetadata))
+	C.StartProfilingPytorch(p.ctx, cname, cmetadata)
+	return nil
 
 }
 
 func (p *Predictor) EndProfiling() error {
-  C.EndProfilingPytorch(p.ctx)
-  return nil
+	C.EndProfilingPytorch(p.ctx)
+	return nil
 }
 
 func (p *Predictor) DisableProfiling() error {
-  C.DisableProfilingPytorch(p.ctx)
-  return nil
+	C.DisableProfilingPytorch(p.ctx)
+	return nil
 }
 
 func (p *Predictor) ReadProfile() (string, error) {
-  cstr := C.ReadProfilePytorch(p.ctx)
-  if cstr == nil {
-    return "", errors.New("failed to read nil profile")
-  }
-  defer C.free(unsafe.Pointer(cstr))
-  return C.GoString(cstr) nil
+	cstr := C.ReadProfilePytorch(p.ctx)
+	if cstr == nil {
+		return "", errors.New("failed to read nil profile")
+	}
+	defer C.free(unsafe.Pointer(cstr))
+	return C.GoString(cstr), nil
 
 }
